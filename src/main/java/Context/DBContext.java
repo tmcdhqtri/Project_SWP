bf207aafa99e178b96654f2ba3e23442b37ba412
Tran Manh Cuong <cunbeo04012017@gmail.com>
2022-10-24 22:57:52 +0700

    Update DB Context

---
+++ src/main/java/Context/DBContext.java
@@ -0,0 +1,30 @@
+package Context;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DBContext{
+    
+    /*USE BELOW METHOD FOR YOUR DATABASE CONNECTION FOR BOTH SINGLE AND MULTILPE SQL SERVER INSTANCE(s)*/
+ /*DO NOT EDIT THE BELOW METHOD, YOU MUST USE ONLY THIS ONE FOR YOUR DATABASE CONNECTION*/
+    public Connection getConnection() throws Exception {
+        String url = "jdbc:sqlserver://" + serverName + ":" + portNumber + "\\" + instance + ";databaseName=" + dbName;
+        if (instance == null || instance.trim().isEmpty()) {
+            url = "jdbc:sqlserver://" + serverName + ":" + portNumber + ";databaseName=" + dbName;
+        }
+        Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+        return DriverManager.getConnection(url, userID, password);
+    }
+    private final String serverName = "CUONGTM\\CUONGTM";
+    private final String dbName = "MyShop";
+    private final String portNumber = "1433";
+    private final String instance = "";//LEAVE THIS ONE EMPTY IF YOUR SQL IS A SINGLE INSTANCE
+    private final String userID = "sa";
+    private final String password = "@Manhcuong22";
+    public static void main(String[] args) {
+        try {
+            System.out.println(new DBContext().getConnection());
+        } catch (Exception e) {
+        }
+    }
+}
