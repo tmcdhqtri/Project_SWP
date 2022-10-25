82272de787ee7eaa0cc3616bad19bde2129d3cd2
HieuTND <hieutndde160581@fpt.edu.vn>
2022-10-25 14:19:18 +0700

    add empty DAO

---
+++ src/main/java/DAO/DAO.java
@@ -0,0 +1,29 @@
+
+package DAO;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+
+public class DAO {
+    Connection con = null;
+    PreparedStatement stm = null;
+    ResultSet rs = null;
+    
+    //BEGIN DAO Customer   
+    //END DAO Customer
+    
+    //BEGIN DAO Food 
+    //END DAO Food
+    
+    //BEGIN DAO Order
+    //END DAO Order
+    
+    //BEGIN DAO OrderDetail
+    //END DAO OrderDetail
+    
+    //BEGIN DAO User
+    //END DAO User
+    
+}
