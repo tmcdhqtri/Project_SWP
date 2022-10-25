8bab8c75eec3f37fe4a8eb8e3e1539a10f3e84d6
IAmBill030202 <nhantvde160428@fpt.edu.vn>
2022-10-25 13:36:08 +0700

    Add model

--- src/main/java/Context/DBContext.java
+++ src/main/java/Context/DBContext.java
@@ -20,7 +20,7 @@ public class DBContext{
     private final String portNumber = "1433";
     private final String instance = "";//LEAVE THIS ONE EMPTY IF YOUR SQL IS A SINGLE INSTANCE
     private final String userID = "sa";
-    private final String password = "@Manhcuong22";
+    private final String password = "123456";
     public static void main(String[] args) {
         try {
             System.out.println(new DBContext().getConnection());
---
+++ src/main/java/Model/Customer.java
@@ -0,0 +1,85 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Model;
+
+/**
+ *
+ * @author Admin
+ */
+public class Customer {
+    private int customerID;
+    private String customerName;
+    private String customerPhone;
+    private String customerEmail;
+    private String customerAddress;
+    private String customerBirthday;
+    private String customerUsername;
+    private String customerPassword;
+
+    public int getCustomerID() {
+        return customerID;
+    }
+
+    public void setCustomerID(int customerID) {
+        this.customerID = customerID;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public String getCustomerPhone() {
+        return customerPhone;
+    }
+
+    public void setCustomerPhone(String customerPhone) {
+        this.customerPhone = customerPhone;
+    }
+
+    public String getCustomerEmail() {
+        return customerEmail;
+    }
+
+    public void setCustomerEmail(String customerEmail) {
+        this.customerEmail = customerEmail;
+    }
+
+    public String getCustomerAddress() {
+        return customerAddress;
+    }
+
+    public void setCustomerAddress(String customerAddress) {
+        this.customerAddress = customerAddress;
+    }
+
+    public String getCustomerBirthday() {
+        return customerBirthday;
+    }
+
+    public void setCustomerBirthday(String customerBirthday) {
+        this.customerBirthday = customerBirthday;
+    }
+
+    public String getCustomerUsername() {
+        return customerUsername;
+    }
+
+    public void setCustomerUsername(String customerUsername) {
+        this.customerUsername = customerUsername;
+    }
+
+    public String getCustomerPassword() {
+        return customerPassword;
+    }
+
+    public void setCustomerPassword(String customerPassword) {
+        this.customerPassword = customerPassword;
+    }
+    
+}
