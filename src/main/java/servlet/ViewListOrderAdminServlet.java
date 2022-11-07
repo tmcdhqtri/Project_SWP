d7faa4eb774b4868929efb3432faf51710ea41f3
HieuTND <hieutndde160581@fpt.edu.vn>
2022-11-07 14:16:16 +0700

    Add view list order Admin servlet

--- src/main/java/servlet/ViewListOrderAdminServlet.java
+++ src/main/java/servlet/ViewListOrderAdminServlet.java
@@ -1,27 +1,18 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
- */
 package servlet;
 
 import DAO.DAO;
 import Model.Order;
-import Model.OrderDetail;
 import java.io.IOException;
+import java.io.PrintWriter;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.util.ArrayList;
-import java.util.List;
 
-/**
- *
- * @author This PC
- */
-public class ViewListOrderServlet extends HttpServlet {
+public class ViewListOrderAdminServlet extends HttpServlet {
 
-    @Override
+   @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
@@ -31,7 +22,7 @@ public class ViewListOrderServlet extends HttpServlet {
         ArrayList<Order> aAllOrder = dao.getAllOrders();
         
         request.setAttribute("aAllOrder", aAllOrder);
-        request.getRequestDispatcher("./Homepage/cusViewOrder.jsp").forward(request, response);
+        request.getRequestDispatcher("./AdminPage/listOrder.jsp").forward(request, response);
     }
 
 
